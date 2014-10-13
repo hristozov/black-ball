e3f48e40d074e00a58a6c0514d1d69999b774ea9
Georgi Hristozov <georgi@forkbomb.nl>
2014-10-13 11:15:01 +0300

    Fixed the question deletion support.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  before_filter :authorize, only: [:delete, :create]
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   # GET /questions
